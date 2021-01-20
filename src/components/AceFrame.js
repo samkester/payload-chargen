2b1496f383f87c70d1061b24d36b9e37d499dc05
Sam Kester <samkester@gmail.com>
2021-01-20 13:39:13 -0700

    item tiles take a list of selectable items

--- src/components/AceFrame.js
+++ src/components/AceFrame.js
@@ -1,9 +1,45 @@
 import styled from "styled-components";
 import AceItem from "./AceItem";
 
+const sampleItem = {
+    name: "Name",
+    slot: "SL",
+    cost: 10000,
+    desc: "This is a sample ACE item description. It might be very long. Though it is not now that strength which in old days moved earth and heavens, that which it is, it is.",
+}
+
+const sampleItemList = [
+    {
+        name: "item 1",
+        slot: "SL",
+        cost: 10000,
+        desc: "This is a sample ACE item description. It might be very long. Though it is not now that strength which in old days moved earth and heavens, that which it is, it is.",
+    },
+    {
+        name: "item 2",
+        slot: "SL",
+        cost: 12000,
+        desc: "This is a sample ACE item description. It might be very long. Though it is not now that strength which in old days moved earth and heavens, that which it is, it is.",
+    },
+    {
+        name: "item 3",
+        slot: "SL",
+        cost: 14000,
+        desc: "This is a sample ACE item description. It might be very long. Though it is not now that strength which in old days moved earth and heavens, that which it is, it is.",
+    },
+    {
+        name: "item 4",
+        slot: "SL",
+        cost: 16000,
+        desc: "This is a sample ACE item description. It might be very long. Though it is not now that strength which in old days moved earth and heavens, that which it is, it is.",
+    },
+]
+
 const StyledFrame = styled.div`
     width: 95%;
-    height: 90vh;
+    min-height: 90vh;
+    margin-top: 2.5vw;
+    margin-bottom: 2.5vw;
     border: 2px solid firebrick;
     border-radius: 5px;
     background-color: silver;
@@ -26,16 +62,7 @@ function AceFrame() {
         <StyledFrame>
             <h1>Your ACE</h1>
             <div>
-                <AceItem />
-                <AceItem />
-                <AceItem />
-                <AceItem />
-                <AceItem />
-                <AceItem />
-                <AceItem />
-                <AceItem />
-                <AceItem />
-                <AceItem />
+                <AceItem list={sampleItemList} />
             </div>
         </StyledFrame>
     );
--- src/components/AceItem.js
+++ src/components/AceItem.js
@@ -1,18 +1,54 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const StyledItem = styled.div`
+    display: flex;
     width: 18%;
     border: 1px solid firebrick;
     border-radius: 5px;
     margin-bottom: 3vh;
+
+    p{
+        font-size: 1.2rem;
+        width: 50%;
+        margin: 0;
+    }
+
+    p.fullwidth{
+        width: 100%;
+    }
+
+    p.title{
+        font-size: 1.6rem;
+    }
+
+    p.paragraph{
+        text-align: left;
+        padding: 0.25em;
+        padding-bottom: 0.5em;
+    }
 `
 
-export default function AceItem() {
+export default function AceItem({list}) {
+    const [num, setNum] = useState(1);
+
+    const item = list[num];
+
+    const setNumHandler = (event) => {
+        setNum(event.target.value)
+    }
+
     return (
         <StyledItem>
-            <h3>Item name</h3>
-            <p>place where the thing goes</p>
-            <p>effect/description</p>
+            <select onChange={setNumHandler} value={num}>
+                {list.map((item, index) => 
+                    <option value={index}>{item.name}</option>
+                )}
+            </select>
+            <p className="fullwidth title">{item?.name}</p>
+            <p>{item?.slot}</p>
+            <p>{item?.cost}</p>
+            <p className="fullwidth paragraph">{item?.desc}</p>
         </StyledItem>
     );
 }
